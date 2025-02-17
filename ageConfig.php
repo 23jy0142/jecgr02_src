30076edd2ea49ea007d88e724d3e578d2c9e7c39
okunoyuyuki <23jy0108@jec.ac.jp>
2025-02-17 16:52:57 +0900

    [update]restart_confirmation.php,ageConfig.php,callingStaff.phpの機能を追加しました。

--- ageConfig.php
+++ ageConfig.php
@@ -1,1 +1,21 @@
-<!-- 年齢確認 -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>年齢確認</title>
+</head>
+<style>
+    body { font-family: Arial, sans-serif; text-align: center; padding: 20px;}
+    h1 { color: #69be86; }
+    .btn { background-color: #9cf;}
+</style>
+
+<body>
+    <h1>年齢確認が必要です</h1>
+    <form action="index.php" method="post">
+        <input type="password" name="password" require />
+        <input type="submit" value="承認" class="btn" />
+    </form>
+</body>
+</html>
